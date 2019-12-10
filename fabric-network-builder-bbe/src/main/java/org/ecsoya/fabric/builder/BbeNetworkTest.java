bd251649090af115af8c8ffefa0a40a893e37f5f
jin.liu@soyatec.com <jin.liu@soyatec.com>
2019-12-10 12:47:26 +0800

    Remove bad dependencies

--- fabric-network-builder-bbe/src/main/java/org/ecsoya/fabric/builder/BbeNetworkTest.java
+++ fabric-network-builder-bbe/src/main/java/org/ecsoya/fabric/builder/BbeNetworkTest.java
@@ -3,7 +3,7 @@ package org.ecsoya.fabric.builder;
 import java.io.File;
 import java.io.IOException;
 
-import org.ecsoya.fabric.network.FabricNetwork;
+import org.hyperledger.fabric.sdk.NetworkConfig;
 import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
 import org.hyperledger.fabric.sdk.exception.NetworkConfigurationException;
 
@@ -13,7 +13,7 @@ public class BbeNetworkTest {
 		File file = new File("src/main/resources/bbe/network/connection.yml");
 
 		try {
-			FabricNetwork network = FabricNetwork.fromYamlFile(file);
+			NetworkConfig network = NetworkConfig.fromYamlFile(file);
 			System.out.println(network);
 		} catch (InvalidArgumentException e) {
 			e.printStackTrace();
